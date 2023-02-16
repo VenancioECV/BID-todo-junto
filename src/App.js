d8d224169abc0303a852a30e1add509f4e107925
Venancio Caceres <venancio.caceres03@gmail.com>
2023-02-16 16:06:50 -0300

    Poniendo todo junto

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <>
       <div className='App'>
-        <PersonCard firstName = {"Jane"} lastName={"Doe"} age={"45"} hairColor={"brown"} />
-        <PersonCard firstName = {"John"} lastName={"Smith"} age={"88"} hairColor={"brown"} />
-        <PersonCard firstName = {"Millard"} lastName={"Fillmore"} age={"50"} hairColor={"brown"} />
-        <PersonCard firstName = {"Maria"} lastName={"Smith"} age={"62"} hairColor={"brown"} />
+        <PersonCard firstName = {"Jane"} lastName={"Doe"} age={45} hairColor={"brown"}></PersonCard>
+        <PersonCard firstName = {"John"} lastName={"Smith"} age={88} hairColor={"brown"}></PersonCard>
+        <PersonCard firstName = {"Millard"} lastName={"Fillmore"} age={50} hairColor={"brown"}></PersonCard>
+        <PersonCard firstName = {"Maria"} lastName={"Smith"} age={62} hairColor={"brown"}></PersonCard>
       </div>
     </>
   );
--- src/components/personCard.js
+++ src/components/personCard.js
@@ -1,12 +1,26 @@
 import React, { Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age : this.props.age,
+        }
+    }
+    IncreaseAge = () => {
+        let actualAge = this.state.age;
+        actualAge = Number(actualAge)
+        let newAge = actualAge;
+        newAge++;
+        this.setState({age : newAge});
+    }
     render(){
         return(
             <div className='PersonCard'>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.IncreaseAge}>Birthday button for {this.props.firstName}</button>
             </div>
         )
     }
